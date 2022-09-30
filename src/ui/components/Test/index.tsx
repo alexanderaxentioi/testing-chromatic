1f2d82875cf86d6b9238135f312efe2f8be95b3d
alexanderaxentioi <101560586+alexanderaxentioi@users.noreply.github.com>
2022-09-30 11:06:47 +0300

    test

--- src/ui/components/Test/index.tsx
+++ src/ui/components/Test/index.tsx
@@ -2,7 +2,13 @@ import * as S from './styles'
 
 const Test = () => (
   <S.Wrapper>
-    <h1>Test</h1>
+    <h2>Test</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam odit
+      ducimus quidem similique facere ipsum dolores reiciendis! Provident
+      voluptates autem, culpa temporibus molestiae consectetur amet obcaecati
+      deleniti expedita suscipit possimus?
+    </p>
   </S.Wrapper>
 )
 
