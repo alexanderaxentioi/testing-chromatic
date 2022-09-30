7ae390e839346eacac9a263bc17325629cae155e
alexanderaxentioi <101560586+alexanderaxentioi@users.noreply.github.com>
2022-09-30 11:21:05 +0300

    test

--- src/ui/components/Test/index.tsx
+++ src/ui/components/Test/index.tsx
@@ -9,6 +9,12 @@ const Test = () => (
       voluptates autem, culpa temporibus molestiae consectetur amet obcaecati
       deleniti expedita suscipit possimus?
     </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam odit
+      ducimus quidem similique facere ipsum dolores reiciendis! Provident
+      voluptates autem, culpa temporibus molestiae consectetur amet obcaecati
+      deleniti expedita suscipit possimus?
+    </p>
   </S.Wrapper>
 )
 
--- src/ui/components/Test/styles.ts
+++ src/ui/components/Test/styles.ts
@@ -1,3 +1,12 @@
 import styled from 'styled-components'
 
-export const Wrapper = styled.main``
+export const Wrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: aliceblue;
+  padding: 20px;
+  font-size: 1cqw;
+  max-width: 30cm;
+  margin: 0 auto;
+`
